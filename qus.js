ac93a9d7ad3269b7aef355e9b603b536c8f047b3
Roam Tang <roam.tang@sap.com>
2016-04-08 17:33:04 +0800

    add first page

--- qus.js
+++ qus.js
@@ -1,5 +1,3 @@
-#   问题
-``` javascript
 function Foo() {
     getName = function () { console.log (1); };
     return this;
@@ -8,13 +6,3 @@ Foo.getName = function () { console.log (2);};
 Foo.prototype.getName = function () { console.log (3);};
 var getName = function () { console.log (4);};
 function getName() { console.log (5);}
-
-//请写出以下输出结果：
-Foo.getName();
-getName();
-Foo().getName();
-getName();
-new Foo.getName();
-new Foo().getName();
-new new Foo().getName();
-```
